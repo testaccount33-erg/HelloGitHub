5698f6c33476fc91a12ef52ced4f3e65e1013590
Ergin, Huseyin <hergin@bsu.edu>
2020-08-31 14:26:14 -0400

    Extracted function.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        sayHello();
+    }
+
+    public static void sayHello() {
+        System.out.println("Hello, World!");
     }
 }
