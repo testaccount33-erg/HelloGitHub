a07404a91a6234858b361e69873d0e0541bd7f33
Ergin, Huseyin <hergin@bsu.edu>
2020-08-31 17:01:11 -0400

    Added a new fancy method!

--- src/Main.java
+++ src/Main.java
@@ -6,4 +6,8 @@ public class Main {
     public static void sayHello() {
         System.out.println("Hello, World!");
     }
+
+    public static void sayHi() {
+        System.out.println("Hi");
+    }
 }
